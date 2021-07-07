fc9b9669ea4138e7ec63cbc00c6c21f90b4ad8cd
10ankit01 <86761781+10ankit01@users.noreply.github.com>
2021-07-07 13:51:20 +0530

    Update signup_script.php

--- signup_script.php
+++ signup_script.php
@@ -1,25 +1,34 @@
 <?php
-include 'includes/common.php';
-$email = mysqli_real_escape_string($con,$_POST['email']);
-$select_query = "SELECT id FROM users Where email = '$email'";
+ include 'includes/common.php';
+if(!$_SESSION['email']){
+    header('location:index.php');
+    exit();
+}
+$user_id = $_SESSION['id'];
+$oldPassword = mysqli_real_escape_string($con , $_POST['oldPW']);
+$newPassword = mysqli_real_escape_string($con , $_POST['newPW']);
+$newPassword1 = mysqli_real_escape_string($con , $_POST['newPW1']);
+$email = $_SESSION['email'];
+if(strlen($newPassword) != strlen($newPassword1)){
+     
+     echo 'ERROR , old password and new password does not match.';  
+      header('Refresh:5 ; url= settings.php');
+   
+}
+else{
+    $select_query = "select id from users where email = '$email' and password = '$oldPassword'";
 $select_query_result = mysqli_query($con, $select_query);
-$num_rows = mysqli_num_rows($select_query_result);
-if($num_rows > 0 ){
-    echo "ERROR , Email already exists  <br>";
-    echo  '<a href="index.php">click here </a>'; 
+if(mysqli_num_rows($select_query_result)== 0){
+    echo 'ERROR , INCORRECT OLD PASSWORD.';  
+    
+    header('Refresh:5 ; url= settings.php');
+   
 }else{
-    
-    $name = mysqli_real_escape_string($con,$_POST['name']);
-$password = mysqli_real_escape_string($con,$_POST['password']);
-$phone = $_POST['phone'];
-$city = mysqli_real_escape_string($con,$_POST['city']);
-$address = mysqli_real_escape_string($con,$_POST['address']);
-$user_registration_query = "insert into users(email, name, password, contact , city , address) values ('$email', '$name', '$password', '$phone','$city' , '$address')";
-$user_registration_submit = mysqli_query($con, $user_registration_query) or die(mysqli_error($con));
-$_SESSION['email'] = $email;
-$_SESSION['id'] = mysqli_insert_id($con);
-header('location:product.php');
-    
+    $update_query = "update users set password = '$newPassword' where email = '$email' and id = '$user_id'";
+    $update_query_result = mysqli_query($con, $update_query) or die(mysqli_error($con));
+    header('location:product.php');
 }
 
+}
 ?>
+
